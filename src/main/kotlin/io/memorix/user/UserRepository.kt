ab8203064b92743bb7a1a3765e6e8bf0aba1a18a
BasBrunink <115614124+BasBrunink@users.noreply.github.com>
2025-01-15 22:23:36 +0100

    In mem done...

--- src/main/kotlin/io/memorix/user/UserRepository.kt
+++ src/main/kotlin/io/memorix/user/UserRepository.kt
@@ -7,17 +7,17 @@ class UserRepository(
 ) {
 
     private val users = mutableListOf(
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
-        User("T1", "t@r.bl", "Fiets12!"),
+        User("Bas", "t@r.bl", "Fiets12!"),
+        User("Maaike", "t@r.bl", "Fiets12!"),
+        User("Gwen", "t@r.bl", "Fiets12!"),
+        User("Emily", "t@r.bl", "Fiets12!"),
+        User("Ruud", "t@r.bl", "Fiets12!"),
+        User("Rini", "t@r.bl", "Fiets12!"),
+        User("Bennie", "t@r.bl", "Fiets12!"),
+        User("Hetty", "t@r.bl", "Fiets12!"),
+        User("Stef", "t@r.bl", "Fiets12!"),
+        User("Suus", "t@r.bl", "Fiets12!"),
+        User("admin", "t@r.bl", "Fiets12!"),
 
     )
 
